c93b6c44ae8108c43819225c522e94a91e7457bb
Tiffany L. Bacon / Sa'Ra' Thiel AuraNova <tiffanylbacon@gmail.com>
2025-10-16 18:18:44 -0700

    fix: Tailwind v4 migration successful

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}","./components/**/*.{js,ts,jsx,tsx,mdx}","./pages/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: { extend: {} },
   plugins: [],
-}
-
+};
